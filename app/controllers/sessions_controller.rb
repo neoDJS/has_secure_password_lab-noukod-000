262438c3ec733d1a07b6fc05b2357308e2d8c12e
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-29 20:40:17 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+  end
+
+  def destroy
+    session.delete :name
+  end
+end
